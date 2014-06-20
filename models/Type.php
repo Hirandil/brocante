32c5a8b9823fbbea9d6bed1dd5e5a382c9e886e7
abrantes01 <abrantes.kevin@gmail.com>
2014-06-20 11:35:31 +0200

    [ADD] Types

---
+++ models/Type.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: abrantes
+ * Date: 20/06/14
+ * Time: 10:27
+ */
+
+    class Type
+    {
+        private $_id;
+        private $_libelle;
+
+        public function __construct (array $data){
+            $this->_id = $data['idType'];
+            $this->_libelle = $data['libelle'];
+        }
+
+        /**
+         * @param mixed $id
+         */
+        public function setId($id)
+        {
+            $this->_id = $id;
+        }
+
+        /**
+         * @return mixed
+         */
+        public function getId()
+        {
+            return $this->_id;
+        }
+
+        /**
+         * @param mixed $libelle
+         */
+        public function setLibelle($libelle)
+        {
+            $this->_libelle = $libelle;
+        }
+
+        /**
+         * @return mixed
+         */
+        public function getLibelle()
+        {
+            return $this->_libelle;
+        }
+
+
+
+    }
+?>
---
+++ models/typeManager.php
@@ -0,0 +1,62 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: abrantes
+ * Date: 20/06/14
+ * Time: 10:59
+ */
+
+include_once('models/Type.php');
+
+class typeManger{
+
+    private $_db;
+
+    public function __construct(PDO $db){
+        $this->_db = $db;
+    }
+
+    public function create(Type $t){
+        $q = $this->_db->prepare('INSERT INTO Types (libelle) VALUES (:libelle)');
+        $q->bindValue(':libelle', $t->getLibelle(), PDO::PARAM_STR);
+        try{
+            $q->execute();
+        }
+        catch(Exception $e)
+        {
+            echo "Error at type creation";
+        }
+    }
+
+    public function exists($id){
+        $q = $this->_db->prepare('SELECT COUNT(*) FROM Types WHERE id = :id');
+        $q->bindValue(':id',$id,PDO::PARAM_INT);
+        $q->execute();
+        if (!$q->fetchColumn(0))
+            return false;
+        else
+            return true;
+    }
+
+    public function get($id){
+        $q = $this->_db->prepare('SELECT * FROM Types WHERE id = :id');
+        $q->bindValue(':id', $id, PDO::PARAM_INT);
+        $q->execute();
+        $data = $q->fetch(PDO::FETCH_ASSOC);
+        $t = new Type($data);
+
+        return $t;
+    }
+
+    public function destroy($id){
+        $q = $this->_db->prepare('DELETE FROM Types WHERE id = :id');
+        $q->bindValue(':id',$id,PDO::PARAM_INT);
+        $q->execute();
+    }
+
+    public function update(Type $t){
+        $q = $this->_db->prepare('UPDATE Types SET libelle= :libelle WHERE id = :id');
+        $q->bindValue(':id', $t->getId(), PDO::PARAM_INT);
+        $q->bindValue(':libelle', $t->getLibelle(), PDO::PARAM_STR);
+    }
+}
