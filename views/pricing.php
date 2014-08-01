babb07fe2a93d477451d911e8e76a2e3eb3fab3d
abrantes01 <abrantes.kevin@gmail.com>
2014-08-01 21:01:54 +0200

    [UPDATE] Paypal

--- views/pricing.php
+++ views/pricing.php
@@ -67,12 +67,18 @@
                             <form action="https://www.sandbox.paypal.com/cgi-bin/webscr" method="post" target="_top">
                                 <input type="hidden" name="cmd" value="_s-xclick">
                                 <input type="hidden" name="hosted_button_id" value="6SW2SPULHJN2Y">
-                                <input name="return" type="hidden" value="http://votredomaine/paiementValide.php" />
-                                <input name="cancel_return" type="hidden" value="http://votredomaine/paiementAnnule.php" />
+                                <input name="return" type="hidden" value="/User/updatepro/valid" />
+                                <input name="cancel_return" type="hidden" value="/User/updatepro" />
                                 <input name="notify_url" type="hidden" value="/validationPayment.php" />
                                 <input type="image" src="https://www.sandbox.paypal.com/fr_FR/FR/i/btn/btn_buynowCC_LG.gif" border="0" name="submit" alt="PayPal - la solution de paiement en ligne la plus simple et la plus sécurisée !">
                                 <img alt="" border="0" src="https://www.sandbox.paypal.com/fr_FR/i/scr/pixel.gif" width="1" height="1">
                             </form>
+<!--                            <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">-->
+<!--                                <input type="hidden" name="cmd" value="_s-xclick">-->
+<!--                                <input type="hidden" name="hosted_button_id" value="MZW9FZFDFU3Q6">-->
+<!--                                <input type="image" src="https://www.paypalobjects.com/fr_FR/FR/i/btn/btn_buynowCC_LG.gif" border="0" name="submit" alt="PayPal - la solution de paiement en ligne la plus simple et la plus sécurisée !">-->
+<!--                                <img alt="" border="0" src="https://www.paypalobjects.com/fr_FR/i/scr/pixel.gif" width="1" height="1">-->
+<!--                            </form>-->
                         </div>
                         <!-- /.content -->
                     </div>
